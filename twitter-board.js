39243fd021c41255003807390365d52582d2f01e
John Walker <mail@john-walker.co.uk>
2011-04-19 15:57:46 +0100

    css styling on image for width twitter-board.js updated to poll for new tweets

--- twitter-board.js
+++ twitter-board.js
@@ -2,25 +2,58 @@
 
 (function($) {
 
-    $.twitterboard = function(q, n) {
+    jQuery.fn.exists = function() { return jQuery(this).length > 0; }
+
+    $.twitterboard = function(q, n, r) {
         // encode the hash if the search contains a hash tag
         var query = q.replace("#", "%23");
+
+        if (r != "update") {
+            $('#twitter-board *').remove();
+        }
+
+        var tweetsAdded = 0;
 
         //Get the twitter feed based on query and no results requested - create an li containing tweet and append to ul
         $.getJSON('http://search.twitter.com/search.json?callback=?&q=' + query + '&rpp=' + n, function(data) {
             $.each(data.results, function(i, item) {
-                var tweet = '<li class=tweet' + i + '>';
+                var tweet = '<li id="' + item.id_str + '">';
                 tweet += '<img src=' + item.profile_image_url + '></img>';
                 tweet += '<a href="http://twitter.com/' + item.from_user + '" class="user">' + item.from_user + '</a><span>Says</span>';
                 tweet += '<p class="tweet">' + $.Linkify(item.text) + '</p>';
                 tweet += '<abbr class="timeago" title=\'' + $.ISODateString(new Date(item.created_at)) + '\'>' + new Date(item.created_at) + '</abbr>';
                 tweet += '</li>';
-                $('ul#twitter-board').append(tweet);
+
+                //Check to see whether the request is an update or initial load
+                //If its not an update append but if its update prepend and remove last item.
+                if (r != "update") {
+                    $('#twitter-board').append(tweet);
+                }
+                else if (!$('#twitter-board li#' + item.id_str).exists()) {
+                    $('#twitter-board li').last().fadeOut("slow").remove();
+                    $(tweet).hide().prependTo("#twitter-board").fadeIn("slow");
+                    tweetsAdded++;
+                }
             });
 
-            $("abbr.timeago").timeago();
+            if (tweetsAdded) {
 
-            $('#twitter-board').sameHeights(10);
+            }
+
+            //test to see if IE then if greater than version 6 run timeago
+            if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)) { //test for MSIE x.x;
+                var ieversion = new Number(RegExp.$1) // capture x.x portion and store as a number
+                if (ieversion > 6)
+                    $("abbr.timeago").timeago();
+            }
+            else {
+                $("abbr.timeago").timeago();
+            }
+
+            if (r != "update" || tweetsAdded) {
+                $('#twitter-board').sameHeights(10);
+            }
+
 
         });
     };
@@ -47,7 +80,8 @@
             $(this).children().each(function(i) {
                 if (tallest < $(this).height()) { tallest = $(this).height(); }
             });
-            $(this).children().css({ 'height': tallest + pad });
+            $(this).children().css({ 'height': tallest + pad + 'px' });
+            //$(this).children().height(tallest);
         });
         return this;
     };
@@ -69,5 +103,26 @@
     };
 
 
+    /* Call this function on every onkeyup event . */
+    $.charactorCount = function twitter_type_count(input_selector, max_chars, update_container_selector) {
+        var text = $(input_selector).val() + "";
+        var curr_num_chars = text.length;
+
+        if (curr_num_chars > max_chars) {
+            $(update_container_selector).addClass('error');
+            $('#tweetSubmit').attr("disabled", true);
+            $('#tweetSubmit').addClass('disabled');
+        }
+        else {
+            $(update_container_selector).removeClass('error');
+            $('#tweetSubmit').attr("disabled", false);
+            $('#tweetSubmit').removeClass('disabled');
+        }
+
+        var leftOver = max_chars - curr_num_chars;
+        $(update_container_selector).html(leftOver);
+        return leftOver;
+    };
 
 })(jQuery);
+
